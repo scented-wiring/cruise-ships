8e1bbb463076d4b919c35c8be8f88b0da04f1efd
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-07-10 10:20:09 +0100

    added itinerary

--- src/Ship.js
+++ src/Ship.js
@@ -1,13 +1,28 @@
-function Ship(port) {
-    this.currentPort = port;
-}
+function Ship(itinerary) {
+    this.itinerary = itinerary;
+    this.currentPort = itinerary.ports[0];
+    this.previousPort = null;
+    Ship.prototype = {
 
-Ship.prototype.setSail = function() {
-    this.currentPort = null;
-}
+        setSail() {
+            const itinerary = this.itinerary;
+            const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
 
-Ship.prototype.dock = function(port) {
-    this.currentPort = port;
+            if (currentPortIndex === (itinerary.ports.length - 1)) {
+                throw new Error('End of itinerary reached');
+            }
+
+            this.previousPort = this.currentPort;
+            this.currentPort = null;
+        },
+
+        dock() {
+            const itinerary = this.itinerary;
+            const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+        
+            this.currentPort = itinerary.ports[previousPortIndex + 1];
+            }
+    }
 }
 
 module.exports = Ship;
